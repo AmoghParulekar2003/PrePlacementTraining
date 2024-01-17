a83597561ad921c0ab53f0bfe0ce11dd677518bb
AmoghParulekar2003 <119802348+AmoghParulekar2003@users.noreply.github.com>
2024-01-17 10:48:20 +0530

    Add files via upload

--- Day3/P1.java
+++ Day3/P1.java
@@ -1,1 +1,19 @@
-
+class P1 {
+    public static void main(String[] args) {
+        try {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = a / b;
+            System.out.println("Result: "+(a+b));
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Result: "+(5+6));
+        }
+        catch(NumberFormatException fe) {
+            System.out.println("Result: "+(8+14));
+        }
+        catch(ArithmeticException ae) {
+            System.out.println("Result: "+(11+22));
+        }
+    }
+}
