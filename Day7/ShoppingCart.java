4afe38f32736f9a859e8af2d118c4c83f93d1f11
AmoghParulekar2003 <119802348+AmoghParulekar2003@users.noreply.github.com>
2024-03-13 21:48:03 +0530

    Update ShoppingCart.java

--- Day7/ShoppingCart.java
+++ Day7/ShoppingCart.java
@@ -6,71 +6,112 @@
 //orderMenu()
 //exit()
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ShoppingCart {
 
-    ArrayList itemNo = new ArrayList();
-    ArrayList itemName = new ArrayList();
-    ArrayList  price = new ArrayList();
+    ArrayList<Integer> itemNo = new ArrayList<>();
+    ArrayList<String> itemName = new ArrayList<>();
+    ArrayList<Integer> price = new ArrayList<>();
 
     public void addItems() {
-        System.out.println("Add items to the cart");
-        for(int i = 0; i < 100; i++) {
-            itemNo.add(i);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Add items to the cart (enter '0' for itemNo to stop):");
+        int currentItemNo;
+        while ((currentItemNo = sc.nextInt()) != 0) {
+            itemNo.add(currentItemNo);
             itemName.add(sc.next());
-            price.add(sc.nextint());
+            price.add(sc.nextInt());
         }
     }
 
     public void showItems() {
-
+        System.out.println("Items in the cart:");
+        for (int i = 0; i < itemNo.size(); i++) {
+            System.out.println((i + 1) + "\t" + itemName.get(i) + "\t" + price.get(i));
+        }
     }
 
     public void deleteItems() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the item number to delete:");
+        int itemNoToDelete = sc.nextInt();
 
+        int index = itemNo.indexOf(itemNoToDelete);
+        if (index != -1) {
+            itemNo.remove(index);
+            itemName.remove(index);
+            price.remove(index);
+            System.out.println("Item removed from the cart.");
+        } else {
+            System.out.println("Item not found in the cart.");
+        }
     }
 
-    public void updateItem() {
+    public void updateItems() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the item number to update:");
+        int itemNoToUpdate = sc.nextInt();
 
+        int index = itemNo.indexOf(itemNoToUpdate);
+        if (index != -1) {
+            System.out.print("Enter new Item Name: ");
+            itemName.set(index, sc.next());
+            System.out.print("Enter new Item Price: ");
+            price.set(index, sc.nextInt());
+            System.out.println("Item updated in the cart.");
+        } else {
+            System.out.println("Item not found in the cart.");
+        }
     }
 
     public void orderMenu() {
-        System.out.println("Here's what you have ordered so far");
+        System.out.println("Here's what you have ordered so far:");
         int totalPrice = 0;
-        for (int i = 0; i < 100; i++) {
-            System.out.println((i+1)+"\t"+itemName.get(i).toString()+"\t"+price.get(i).toString());
-            totalPrice = totalPrice + price.get(i);
+        for (int i = 0; i < itemNo.size(); i++) {
+            System.out.println((i + 1) + "\t" + itemName.get(i) + "\t" + price.get(i));
+            totalPrice += price.get(i);
         }
-        System.out.println("Your total bill is: " +totalPrice);
+        System.out.println("Your total bill is: $" + totalPrice);
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
         ShoppingCart sp = new ShoppingCart();
-        System.out.println("Welcome to Amazon.com's shopping cart!");
-        System.out.println("Please select from the following options:");
-        System.out.println("\t1. Add an Item to your Cart.");
-        System.out.println("\t2. Show Items in your Cart.");
-        System.out.println("\t3. Remove an Item from your Cart.");
-        System.out.println("\t4. Update Quantity of an Item in your Cart.");
-        System.out.println("\t5. Order Menu.");
-        System.out.println("\t6. Exit Application.\n");
-        int choice = sc.nextInt();
 
-        while(true) {
-            switch(choice) {
-                case 1: sp.addItems();
-                break;
-                case 2: sp.showItems();
-                break;
-                case 3: sp.deleteItems();
-                break;
-                case 4: sp.updateItems();
-                break;
-                case 5: sp.orderMenu();
-                break;
-                case 6: System.exit(1);
-                break;
+        while (true) {
+            System.out.println("\nPlease select from the following options:");
+            System.out.println("1. Add an Item to your Cart");
+            System.out.println("2. Show Items in your Cart");
+            System.out.println("3. Remove an Item from your Cart");
+            System.out.println("4. Update Quantity of an Item in your Cart");
+            System.out.println("5. Order Menu");
+            System.out.println("6. Exit Application");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    sp.addItems();
+                    break;
+                case 2:
+                    sp.showItems();
+                    break;
+                case 3:
+                    sp.deleteItems();
+                    break;
+                case 4:
+                    sp.updateItems();
+                    break;
+                case 5:
+                    sp.orderMenu();
+                    break;
+                case 6:
+                    System.out.println("Exiting the application. Thank you!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
             }
         }
     }
