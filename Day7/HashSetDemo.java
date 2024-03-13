6ff5164d1f9f1fd2861258583d5fbfb1c721a22e
AmoghParulekar2003 <119802348+AmoghParulekar2003@users.noreply.github.com>
2024-03-13 16:06:13 +0530

    Add files via upload

---
+++ Day7/HashSetDemo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        HashSet<String> h = new HashSet<>();
+        h.add("A");
+        h.add("B");
+        h.add("C");
+        h.add("D");
+        h.add("D");
+
+        Iterator<String> itr = h.iterator();
+        while(itr.hasNext()) {
+            String str = itr.next();
+            System.out.println(str);
+        }
+        System.out.println(h);
+    }
+}
---
+++ Day7/TreeSetDemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet ts = new TreeSet();
+        ts.add("A");
+        ts.add("Z");
+        ts.add("D");
+        ts.add("C");
+        ts.add("B");
+        System.out.println(ts);
+    }
+}
